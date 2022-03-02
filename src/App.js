561aa57181edb2837e442d715cf0db6d4ddcf56b
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2022-03-02 18:39:05 +0900

    feat: button for erasing all todoData

--- src/App.js
+++ src/App.js
@@ -18,12 +18,23 @@ export default function App () {
     setTodoData([...todoData, newTodo]);
     setValue("");
   };
+
+  const erasor = () => {
+    let emptyTodo = [];
+    setTodoData(emptyTodo);
+  }
   
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
       <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
         <div className="flex justify-between mb-3">
-          <h1>할 일 목록</h1>
+          <h1 className=''>할 일 목록</h1>
+          <button
+            className="p-2 hover:text-white hover:bg-blue-200 text-blue-400 border-2  border-blue-400 rounded"
+            onClick={erasor}
+          >
+            모두 지우기
+          </button>
         </div>
         <List todoData={todoData} setTodoData={setTodoData} />
         <Form value={value} setValue={setValue} handleSubmit={handleSubmit} />
