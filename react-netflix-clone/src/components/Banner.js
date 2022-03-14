fe208417a839229de9edbd7ecfafe58cd69921bc
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2022-03-14 23:33:14 +0900

    feat: video banner

--- react-netflix-clone/src/components/Banner.js
+++ react-netflix-clone/src/components/Banner.js
@@ -36,7 +36,7 @@ const Banner = () => {
     const movieId = request.data.results[Math.floor(Math.random()*request.data.results.length)].id;
 
     const {data : movieDetail} = await axios.get(`movie/${movieId}`, {
-      params: {append_to_reponse : "videos"},
+      params: {append_to_response : "videos"},
     });
     
     setMovie(movieDetail);
@@ -76,12 +76,42 @@ const Banner = () => {
     );
   }
   else{
-    return (<Container><HomeContainer>clicked</HomeContainer></Container>);
+    return (
+      <Container>
+        <HomeContainer>
+          <Iframe
+            width="640"
+            height="360"
+            src={`https://www.youtube.com/embed/${movie.videos.results[0].key}
+          ?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+            frameborder="0"
+            allow="autoplay; fullscreen"
+          ></Iframe>
+        </HomeContainer>
+      </Container>
+    );
     
   }
 }
 
 // styled Component
+
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index : -1;
+  opacity : 0.65;
+  border : none;
+  &::after {
+    content : "";
+    position: absolute;
+    top:0;
+    left:0;
+    width:100%;
+    height:100%;
+  }
+`;
+
 const Container = styled.div`
   display:flex;
   justify-content:center;
